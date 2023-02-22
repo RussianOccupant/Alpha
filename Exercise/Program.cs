95992a12e6d964a781d3ff3b910f4d4269d048ca
AlexBystrov <sshbystrov@gmail.com>
2023-02-22 21:04:22 +0400

    Added for and count

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,1 +1,7 @@
 ﻿string[] array = {"Hello", "World", "and", "two", "my", "favorite", "friends", "!"};
+Console.Write("[ ");
+
+for (int i = 0; i < array.Length; i++)
+{
+    string count = array[i];
+}
