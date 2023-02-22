bed79ddf68b7cb18ebbda8200c4fa62339ad8094
AlexBystrov <sshbystrov@gmail.com>
2023-02-22 21:06:07 +0400

    Added if and last border

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -4,4 +4,9 @@ Console.Write("[ ");
 for (int i = 0; i < array.Length; i++)
 {
     string count = array[i];
+    if (count.Length <= 3)
+    {
+        Console.Write($"{count} ");
+    }
 }
+Console.Write("]");
